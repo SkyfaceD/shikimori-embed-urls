7e24983baaac396fe86de12547ed52943f2cb8ba
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-04-23 18:41:20 +0600

    Fix myvi handler

--- src/App.kt
+++ src/App.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
     val sibnet = handleSibnetRedirect("https://dv98.sibnet.ru/38/30/47/3830478.mp4?st=uwS-JHb0GRNJ-kcfAk65Zg&e=1619024000&stor=8&noip=1")
     val myvitv = handleMyviEmbeddedUrl("https://www.myvi.tv/embed/rnaoif1wd5jw3poog11t7cgj8w")
     val myvitop = handleMyviEmbeddedUrl("https://www.myvi.top/embed/z769p4zsfabrxmky7kbm398sne")
+    val myvitop2 = handleMyviEmbeddedUrl("https://myvi.top/embed/hsym36rgn6grjmjtaydi4fciwa")
     //@formatter:on
     readLine()
 }
--- src/Myvi.kt
+++ src/Myvi.kt
@@ -35,43 +35,8 @@ fun handleMyviEmbeddedUrl(stringUrl: String): String {
     if (responseCode != HttpURLConnection.HTTP_OK) return stringUrl
 
     val rawHtml = connection.inputStream.use { readRawHtml(it) }
-
     val extractedVideoUrl = extractVideoUrlFromRawHtml(rawHtml)
-
     val videoUrl = handleMyviRedirect(extractedVideoUrl) ?: throw Exception("Can't redirect")
-
-    return videoUrl
-}
-
-/**
- * Connect to passed [stringUrl] and return valid url if redirect found.
- *
- * @return
- * entry url if [stringUrl] host not myvi or if status code [HttpURLConnection.HTTP_OK],
- * null if location header not found or status code not [HttpURLConnection.HTTP_MOVED_TEMP],
- * valid url otherwise
- */
-@Suppress("UsePropertyAccessSyntax", "UnnecessaryVariable")
-fun handleMyviRedirect(stringUrl: String): String? {
-    val url = URL(stringUrl)
-    if (!url.host.contains("myvi")) return stringUrl
-
-    val connection: HttpsURLConnection = (url.openConnection() as HttpsURLConnection).apply {
-        setConnectTimeout(15_000)
-        setReadTimeout(15_000)
-        setInstanceFollowRedirects(false)
-        addRequestProperty(
-            "User-Agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
-        )
-        disconnect()
-    }
-
-    val responseCode = connection.responseCode
-    if (responseCode == HttpURLConnection.HTTP_OK) return stringUrl
-    if (responseCode != HttpURLConnection.HTTP_MOVED_TEMP) return null
-
-    val videoUrl = connection.getHeaderField("location")
 
     return videoUrl
 }
@@ -99,5 +64,38 @@ private fun extractVideoUrlFromRawHtml(rawHtml: String): String {
         ?.split("(?=https)".toRegex())
         ?.find { it.contains("stream") } ?: throw Exception("Can't find script or url")
     val url = URLDecoder.decode(stringUrl, "UTF-8")
-    return url.substringBefore("\\u0026tp=", "")
+        .substringBefore("\\u0026tp=", "")
+    return if (url.startsWith("//")) "https:".plus(url) else url
+}
+
+/**
+ * Connect to passed [stringUrl] and return valid url if redirect found.
+ *
+ * @return
+ * entry url if [stringUrl] host not myvi or if status code [HttpURLConnection.HTTP_OK],
+ * null if location header not found or status code not [HttpURLConnection.HTTP_MOVED_TEMP],
+ * valid url otherwise
+ */
+@Suppress("UsePropertyAccessSyntax", "UnnecessaryVariable")
+fun handleMyviRedirect(stringUrl: String): String? {
+    val url = URL(stringUrl)
+
+    val connection: HttpsURLConnection = (url.openConnection() as HttpsURLConnection).apply {
+        setConnectTimeout(15_000)
+        setReadTimeout(15_000)
+        setInstanceFollowRedirects(false)
+        addRequestProperty(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
+        )
+        disconnect()
+    }
+
+    val responseCode = connection.responseCode
+    if (responseCode == HttpURLConnection.HTTP_OK) return stringUrl
+    if (responseCode != HttpURLConnection.HTTP_MOVED_TEMP) return null
+
+    val videoUrl = connection.getHeaderField("location")
+
+    return videoUrl
 }
